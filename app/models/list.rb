3a1b2c473a9e51334e21e29b731cbf8467e52c0d
Gulce Sakallioglu <gulcesakallioglu@gmail.com>
2021-11-11 12:05:45 +0000

    validations and associations

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,7 @@
 class List < ApplicationRecord
+  has_many :bookmarks, dependent: :destroy
+  validates_associated :bookmarks
+  has_many :movies, through: :bookmarks
+  validates_associated :movies
+  validates :name, presence: true, uniqueness: true
 end
